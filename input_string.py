5ffd3c3242aa18c5f1051fb752a54fd1fd996350
kaerudes <kalle.tyllila@gmail.com>
2019-03-22 10:28:42 +0200

    Python exercise 1.

---
+++ input_string.py
@@ -0,0 +1,14 @@
+
+
+def input_string ():
+
+    print ("Enter your firstname: (enter)")
+    firstname = input()
+    print ("Enter your lastname: (enter)")
+    lastname = input()
+    print ("Hello "+firstname+" "+lastname)
+
+
+
+
+input_string()
