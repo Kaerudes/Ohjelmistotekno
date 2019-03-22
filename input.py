351132960645670d3ba1cd11770439b07e98b572
kaerudes <kalle.tyllila@gmail.com>
2019-03-22 10:43:07 +0200

    Python Exercise 2.

---
+++ input.py
@@ -0,0 +1,18 @@
+def input_2 ():
+
+    print ("Enter your firstname:")
+    firstname = input()
+    print ("What is your age")
+    age = input()
+    print ("What is your income?")
+    income = input()
+    print( "Here is the data you entered")
+    print("Name:"+firstname)
+    print("age:" + age)
+    print("income:" + income)
+
+
+
+
+
+input_2()
