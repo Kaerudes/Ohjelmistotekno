3796de66ef4cad8a24f227976ea3fdb3813ec885
kaerudes <kalle.tyllila@gmail.com>
2019-04-02 11:37:43 +0300

    Exercise 6, Writing a file.

---
+++ write_sales.py
@@ -0,0 +1,24 @@
+
+
+def write():
+
+    print("For how many days do you have sales?")
+    sale_days = input()
+    sale_days = int(sale_days)
+
+    file_writing = open('sales.txt','w')
+
+    for daysale in range(1, sale_days+1,):
+        print("Enter the sales of the day #",daysale)
+        daysale = input()
+        file_writing.write(daysale+" ")
+
+
+    file_writing.close()
+
+
+
+
+
+
+write()
