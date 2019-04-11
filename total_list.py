e6594a2de87108477fb1fdb613430968972297de
Kaerudes <43025490+Kaerudes@users.noreply.github.com>
2019-04-11 15:12:10 +0300

    Exercise 11 Total_list

---
+++ total_list.py
@@ -0,0 +1,16 @@
+
+
+def total():
+
+    lista = [2,4,6,8,10]
+    summa = 0
+
+    for x in range (len(lista)):
+        summa = summa + lista[x]
+
+
+    print("The total of the eelements is",summa)
+
+
+
+total()
